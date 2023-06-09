8cf17d0eb40fc5313bc42fc41fa5028ae071a289
Nicolò Scalone <nicolo.scalone@gmail.com>
2023-06-09 22:41:24 +0200

    Added Warp feature

--- Assets/Script/Spaceship.cs
+++ Assets/Script/Spaceship.cs
@@ -19,7 +19,15 @@ public class Spaceship : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    // added for wrapping
+	Renderer[] renderers;
+	bool isWrappingX = false;
+	bool isWrappingY = false;
     
+    Transform[] ghosts = new Transform[8];
+	
+	float screenWidth;
+	float screenHeight;
 
     void Awake()
     {
@@ -40,6 +48,21 @@ public class Spaceship : MonoBehaviour
     private void Start()
     {
         currentHealth = SpaceShipSettings.MaxHealth;
+        
+        //  added
+        renderers = GetComponentsInChildren<Renderer>();
+		
+		var cam = Camera.main;
+        
+        var screenBottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, transform.position.z));
+		var screenTopRight = cam.ViewportToWorldPoint(new Vector3(1, 1, transform.position.z));
+		
+		// The width is then equal to difference between the rightmost and leftmost x-coordinates
+		screenWidth = screenTopRight.x - screenBottomLeft.x;
+		// The height, similar to above is the difference between the topmost and the bottom ycoordinates
+		screenHeight = screenTopRight.y - screenBottomLeft.y;
+        // CreateGhostShips();
+
     }
 
 
@@ -60,6 +83,8 @@ public class Spaceship : MonoBehaviour
 
     void Update()
     {
+        ScreenWrap();
+
         // Get input for horizontal movement
         moveInput = Input.GetAxis("Horizontal");
 
@@ -89,6 +114,76 @@ public class Spaceship : MonoBehaviour
             MaxSpeed = targetSpeed;
         }
     }
+    	void ScreenWrap()
+	{	
+		// If all parts of the object are invisible we wrap it around
+		foreach(var renderer in renderers)
+		{
+			if(renderer.isVisible)
+			{
+				isWrappingX = false;
+				isWrappingY = false;
+				return;
+			}
+		}
+
+		// If we're already wrapping on both axes there is nothing to do
+		if(isWrappingX && isWrappingY) {
+			return;
+		}
+		
+		var cam = Camera.main;
+		var newPosition = transform.position;
+		
+		// We need to check whether the object went off screen along the horizontal axis (X),
+		// or along the vertical axis (Y).
+		//
+		// The easiest way to do that is to convert the ships world position to
+		// viewport position and then check.
+		//
+		// Remember that viewport coordinates go from 0 to 1?
+		// To be exact they are in 0-1 range for everything on screen.
+		// If something is off screen, it is going to have
+		// either a negative coordinate (less than 0), or
+		// a coordinate greater than 1
+		//
+		// So, we get the ships viewport position
+		var viewportPosition = cam.WorldToViewportPoint(transform.position);
+		
+		
+		// Wrap it is off screen along the x-axis and is not being wrapped already
+		if (!isWrappingX && (viewportPosition.x > 1 || viewportPosition.x < 0))
+		{
+			// The scene is laid out like a mirror:
+			// Center of the screen is position the camera's position - (0, 0),
+			// Everything to the right is positive,
+			// Everything to the left is negative;
+			// Everything in the top half is positive
+			// Everything in the bottom half is negative
+			// So we simply swap the current position with it's negative one
+			// -- if it was (15, 0), it becomes (-15, 0);
+			// -- if it was (-20, 0), it becomes (20, 0).
+			newPosition.x = -newPosition.x;
+			
+			// If you had a camera that isn't at X = 0 and Y = 0,
+			// you would have to use this instead
+			// newPosition.x = Camera.main.transform.position - newPosition.x;
+			
+			isWrappingX = true;
+		}
+		
+		// Wrap it is off screen along the y-axis and is not being wrapped already
+		if (!isWrappingY && (viewportPosition.y > 1 || viewportPosition.y < 0))
+		{
+			newPosition.y = -newPosition.y;
+			
+			isWrappingY = true;
+		}
+		
+		//Apply new position
+		transform.position = newPosition;
+	}
+
     
 }
 
